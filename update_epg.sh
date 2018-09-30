5cb60c45165f1aff9ab4e59d951c346f41285224
João Martinho <jdmartinho@gmail.com>
2018-09-30 12:01:52 +0100

    Finished update_epg.sh and added new fashion channels.

--- update_epg.sh
+++ update_epg.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Run this weekly with cron '0 10 * * 6' (Every Saturday at 10:00)
-~/.wg++/run.sh
-# Upload guide.xml file to remote server with scp
-scp ~/.wg++/guide.xml jdmartin@jdmartinho.com:<destination path>
+cp ./WebGrab++.config.xml ~/Downloads/wg++/WebGrab++.config.xml
+~/Downloads/wg++/run.sh
+cp ~/Downloads/wg++/guide.xml ~/Downloads/WG2MP/myguidebefore.xml
+# Run the timeshift script to get the proper timezone changes
+/usr/bin/mono ~/Downloads/WG2MP/WG2MP.exe ~/Downloads/WG2MP/myguidebefore.xml ~/Downloads/WG2MP/guide.xml
 # Upload guide.xml file to remote server with ftp
-HOST='ftp.jdmartinho.com'
-USER='yourid'
-PASSWD='yourpw'
-FILE='~/.wg++/guide.xml'
+HOST='myhost.com'
+USER='myuser'
+PASSWD='mypassword'
+FILE='~/Downloads/WG2MP/guide.xml guide.xml'
 
 ftp -n $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
+cd www
 binary
 put $FILE
 quit
